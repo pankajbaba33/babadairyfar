9586ea51c7ef0edd62c524c57cbf27c5af8e2e56
pankaj <yadav@gmaile.com>
2024-01-28 19:27:21 +0530

    #cont

--- src/component/HomeData/Home.jsx
+++ src/component/HomeData/Home.jsx
@@ -3,7 +3,7 @@ export default function Home()
 {
     return(
        <div className="bg-[#ffa500]">
- <h1 className="text-[23px] text-center text-blue-100">hello pankaj baba i belong to uttar pradesh ghazipur</h1>
+ <h1 className="text-[23px] text-center text-blue-100">hello pankaj yadav baba i belong to uttar pradesh ghazipur</h1>
        </div>
     )
 }
